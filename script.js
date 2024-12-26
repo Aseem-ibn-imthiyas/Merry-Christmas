31c828645ce09068131d28b8b4596c04a76ced7a
Aseem Ibn Imthiyas <Mhdaseem20196@gmail.com>
2024-12-26 10:57:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// VanillaTilt initialization for the boxes
+VanillaTilt.init(document.querySelectorAll('.box'), {
+    max: 25, // Maximum tilt angle
+    speed: 400, // Speed of the tilt
+    glare: true, // Enable glare effect
+    "max-glare": 0.5, // Maximum glare intensity
+});
